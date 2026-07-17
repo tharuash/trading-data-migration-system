92d2cd3446d9cb12ab6ff71bb776521db76f0ee5
Tharindu Aththanayake <tharindu.aththanayake8@gmail.com>
2026-07-18 01:47:39 +0530

    CM-06 Initiated batch migration service

---
+++ trading-data-mangement-solution/src/main/java/org/tlabs/solution/tdm/TradingDataMigrator.java
@@ -0,0 +1,12 @@
+package org.tlabs.solution.tdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingDataMigrator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingDataMigrator.class, args);
+    }
+}
